27d99fbe0cc6ab11095aef75766615eda6daad5f
sjjwantfish <sjjwantfish@163.com>
2023-11-30 11:18:52 +0800

    change nvim-surround config

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -3,11 +3,20 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end,
+    opts = {
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "q",
+        -- visual_line = "Q",
+        delete = "ds",
+        change = "cs",
+      },
+    },
   },
   {
     "numToStr/Comment.nvim",
