58f60fbee2829d1686b4d6a3b8e944d60a847fab
sjjwantfish <sjjwantfish@163.com>
2023-12-14 18:35:05 +0800

    split window.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -72,44 +72,6 @@ return {
       })
     end,
   },
-  {
-    "nyngwang/NeoZoom.lua",
-    opts = {
-      exclude_buftypes = { "terminal" },
-      exclude_filetypes = { "lspinfo", "mason", "lazy", "NvimTree" },
-      -- winopts = {
-      --   offset = {
-      --     -- NOTE: omit `top`/`left` to center the floating window vertically/horizontally.
-      --     -- top = 0,
-      --     -- left = 0.17,
-      --     width = 150,
-      --     height = 0.85,
-      --   },
-      --   -- NOTE: check :help nvim_open_win() for possible border values.
-      --   border = "thicc", -- this is a preset, try it :)
-      -- },
-      presets = {
-        {
-          -- NOTE: regex pattern can be used here!
-          filetypes = { "dapui_.*", "dap-repl" },
-          winopts = {
-            offset = { top = 0.02, left = 0.26, width = 0.74, height = 0.25 },
-          },
-        },
-        {
-          filetypes = { "markdown" },
-          callbacks = {
-            function()
-              vim.wo.wrap = true
-            end,
-          },
-        },
-      },
-    },
-    keys = {
-      { "<leader>wt", "<cmd>NeoZoomToggle<cr>", desc = "NeoZoomToggle" },
-    },
-  },
   { "kevinhwang91/nvim-bqf", opts = {
     preview = {
       winblend = 0,
@@ -147,18 +109,6 @@ return {
       },
       extra_groups = {}, -- table: additional groups that should be cleared
       exclude_groups = {}, -- table: groups you don't want to clear
-    },
-  },
-  {
-    "nvim-zh/colorful-winsep.nvim",
-    opts = {
-      highlight = {
-        -- bg  "#16161E",
-        fg = "#8bd7fd",
-      },
-      interval = 30,
-      no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest" },
-      symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
     },
   },
 }
