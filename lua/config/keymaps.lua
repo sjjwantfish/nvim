3fa65077742e1224095e2640f66f3b4dd0dc7f7b
sjjwantfish <sjjwantfish@163.com>
2024-01-19 17:41:02 +0800

    add user command run cmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,3 +17,35 @@ keymap("i", "<a-h>", "<esc>ha", opts)
 
 -- keymap("n", "za", "zA", opts)
 keymap("n", "gw", "*N", opts)
+
+local function execute_and_print_cmd()
+  -- local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
+  -- local lines = vim.fn.getline(start_pos[2], end_pos[2])
+  -- local selected_text = table.concat(lines, "\n")
+
+  local selected_text = vim.api.nvim_get_current_line()
+  if selected_text ~= "" then
+    local command = "zsh -i -c '" .. selected_text .. "'"
+    vim.notify(command)
+    local handle = io.popen(command)
+    local command_output = handle:read("*a")
+    handle:close()
+    local contents = vim.split(command_output, "\n")
+    if contents[#contents] == "" then
+      table.remove(contents, #contents)
+    end
+    vim.fn.setpos(".", end_pos)
+
+    -- vim.api.nvim_put({ "{>>>>>>>>>>>>>>>>>>>>>>>>>>" }, "l", true, false)
+    vim.fn.setreg("+", contents)
+    vim.api.nvim_put(contents, "l", true, false)
+    -- vim.api.nvim_put({"}<<<<<<<<<<<<<<<<<<<<<<<"}, 'l', true , false)
+  else
+    vim.notify("no text selected")
+  end
+end
+
+vim.api.nvim_create_user_command("RunCommand", function()
+  execute_and_print_cmd()
+end, { nargs = "?", complete = "dir" })
