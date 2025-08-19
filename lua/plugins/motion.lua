f88fe5212383222e1d232e6f07fc6b465943a036
sjjwantfish <sjjwantfish@163.com>
2025-08-19 17:54:30 +0800

    update plugins; add motion

--- lua/plugins/motion.lua
+++ lua/plugins/motion.lua
@@ -210,4 +210,12 @@ return {
       })
     end,
   },
+  {
+    "jinh0/eyeliner.nvim",
+    config = function()
+      require("eyeliner").setup({
+        highlight_on_key = false,
+      })
+    end,
+  },
 }
